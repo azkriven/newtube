fc4668d45928d2f02475fd62217512c2abcf3636
azkriven <azkriven18@gmail.com>
2025-03-01 11:36:59 +0800

    🔧 feat: configure tRPC with auth context, transformers, protectedProcedure, and rate limiting

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,9 @@
 import { HydrateClient, trpc } from "@/trpc/server";
 import { ClientGreeting } from "./client";
-import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorBoundary } from "react-error-boundary";
 import { Suspense } from "react";
 export default async function Home() {
-    void trpc.hello.prefetch({ text: "word" });
+    void trpc.hello.prefetch({ text: "world" });
     return (
         <HydrateClient>
             <ErrorBoundary fallback={<div>Something went wrong</div>}>
